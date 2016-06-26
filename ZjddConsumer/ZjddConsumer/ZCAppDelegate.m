268ef7ff90cc0b458a2cc0e094d451221bc62116
feizheng <1290657123@qq.com>
2016-06-26 20:29:28 +0800

    finish launch screen

--- ZjddConsumer/ZjddConsumer/ZCAppDelegate.m
+++ ZjddConsumer/ZjddConsumer/ZCAppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"App init!");
+    sleep(5);
     return YES;
 }
 
